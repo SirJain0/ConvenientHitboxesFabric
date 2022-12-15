d16b1f8921362e8b8e20fa8911a5072d53985dab
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-14 22:18:23 -0500

    Inject to correct class

--- src/main/java/sirjain/convenient_hitboxes/mixin/ExampleMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ExampleMixin.java
@@ -1,12 +1,12 @@
 package sirjain.convenient_hitboxes.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.GameMenuScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(GameMenuScreen.class)
 public class ExampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
