320e15c75b8b1a52e35bfff788b0f7ec2a06cd65
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-14 22:23:48 -0500

    Inject to proper method
    Fixed class extension too

--- src/main/java/sirjain/convenient_hitboxes/mixin/ExampleMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ExampleMixin.java
@@ -1,14 +1,20 @@
 package sirjain.convenient_hitboxes.mixin;
 
 import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
-public class ExampleMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
+public class ExampleMixin extends Screen {
+    protected ExampleMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(at = @At("HEAD"), method = "initWidgets()V")
     private void init(CallbackInfo info) {
         System.out.println("Mixin initialized");
     }
