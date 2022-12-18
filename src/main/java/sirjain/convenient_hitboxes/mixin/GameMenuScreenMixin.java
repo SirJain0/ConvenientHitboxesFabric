298bcf6842e10c792dd1fd53f21ecb26655a30d1
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-18 14:31:12 -0500

    Add experimental button

--- src/main/java/sirjain/convenient_hitboxes/mixin/GameMenuScreenMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/GameMenuScreenMixin.java
@@ -2,6 +2,7 @@ package sirjain.convenient_hitboxes.mixin;
 
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,5 +18,9 @@ public class GameMenuScreenMixin extends Screen {
     @Inject(at = @At("HEAD"), method = "initWidgets()V")
     private void init(CallbackInfo info) {
         System.out.println("Mixin initialized");
+
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, (this.height / 4 + 120 + -16 + 36), 204, 20,Text.translatable("gui.entity_hitbox"), (button) -> {
+            System.out.println("button clicked");
+        }));
     }
 }
