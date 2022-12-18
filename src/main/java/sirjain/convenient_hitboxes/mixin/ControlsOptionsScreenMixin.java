18bcd25217c7d7f1c5119c24e327d4432427a723
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-18 15:13:17 -0500

    Put button in Controls screen

--- src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
@@ -2,6 +2,7 @@ package sirjain.convenient_hitboxes.mixin;
 
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,17 +10,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GameMenuScreen.class)
-public class GameMenuScreenMixin extends Screen {
-    protected GameMenuScreenMixin(Text title) {
+@Mixin(ControlsOptionsScreen.class)
+public class ControlsOptionsScreenMixin extends Screen {
+    protected ControlsOptionsScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(at = @At("HEAD"), method = "initWidgets()V")
+    @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         System.out.println("Mixin initialized");
 
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, (this.height / 4 + 120 + -16 + 36), 204, 20,Text.translatable("gui.entity_hitbox"), (button) -> {
+        this.addDrawableChild(new ButtonWidget((this.width / 2 - 102) + 107, (this.height / 6 - 12) + 48, 150, 20, Text.translatable("gui.entity_hitbox"), (button) -> {
             System.out.println("button clicked");
         }));
     }
