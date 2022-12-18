5f8ea331a0fd1a85a68aca10714eb678eeb966f6
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-18 18:58:18 -0500

    Send debug message when toggling hitboxes

--- src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
@@ -1,10 +1,10 @@
 package sirjain.convenient_hitboxes.mixin;
 
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +19,7 @@ public class ControlsOptionsScreenMixin extends Screen {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
         assert this.client != null;
-        boolean bl = !this.client.getEntityRenderDispatcher().shouldRenderHitboxes();
+        boolean shouldRenderHitboxes = !this.client.getEntityRenderDispatcher().shouldRenderHitboxes();
         System.out.println("Mixin initialized");
 
         // Adds the button
@@ -28,11 +28,25 @@ public class ControlsOptionsScreenMixin extends Screen {
                 (this.width / 2 - 102) + 107, // x location
                 (this.height / 6 - 12) + 48, // y location
                 150, 20, // button dimensions
-                bl ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"), (button) -> {
-                    this.client.getEntityRenderDispatcher().setRenderHitboxes(bl);
+                shouldRenderHitboxes ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"), (button) -> {
+                    this.client.getEntityRenderDispatcher().setRenderHitboxes(shouldRenderHitboxes);
+                    this.debugLog(shouldRenderHitboxes ? "debug.show_hitboxes.on" : "debug.show_hitboxes.off");
                     System.out.println("button clicked");
                 }
             )
         );
+    }
+
+    private void addDebugMessage(Text text) {
+        assert this.client != null;
+        this.client.inGameHud.getChatHud().addMessage(Text.empty().append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD)).append(" ").append(text));
+    }
+
+    private void debugLog(Text text) {
+        this.addDebugMessage(text);
+    }
+
+    private void debugLog(String key, Object... args) {
+        this.debugLog(Text.translatable(key, args));
     }
 }
