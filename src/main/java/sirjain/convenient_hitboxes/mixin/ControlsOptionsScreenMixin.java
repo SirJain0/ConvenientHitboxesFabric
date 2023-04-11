c4f192ff9951eda3c220595d7aed13c14fcaa245
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-10 20:58:12 -0400

    Port to 1.19.4, prevent possible crashing

--- src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
@@ -1,10 +1,11 @@
 package sirjain.convenient_hitboxes.mixin;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.KeybindsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,32 +14,30 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ControlsOptionsScreen.class)
-public class ControlsOptionsScreenMixin extends Screen {
-    protected ControlsOptionsScreenMixin(Text title) {
-        super(title);
+public class ControlsOptionsScreenMixin extends GameOptionsScreen {
+    public ControlsOptionsScreenMixin(Screen parent, GameOptions gameOptions) {
+        super(parent, gameOptions, Text.translatable("hi"));
     }
 
     @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        assert this.client != null;
+    protected void init(CallbackInfo info) {
+        if (this.client == null) return;
+        System.out.println("Ctrls options screen");
 
-        // Adds the button
-        this.addDrawableChild(
-            new ButtonWidget(
-                (this.width / 2 - 102) + 107, // x location
-                (this.height / 6 - 12) + 48, // y location
-                150, 20, // button dimensions
-                !this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"), (button) -> {
-                    this.client.getEntityRenderDispatcher().setRenderHitboxes(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes());
-                    button.setMessage(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"));
-                    this.debugLog(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? "debug.show_hitboxes.off" : "debug.show_hitboxes.on");
-                }
-            )
-        );
+        this.addDrawableChild(ButtonWidget.builder(
+            !this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"),
+            (button) -> {
+                this.client.getEntityRenderDispatcher().setRenderHitboxes(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes());
+                button.setMessage(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"));
+                this.debugLog(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? "debug.show_hitboxes.off" : "debug.show_hitboxes.on");
+            }
+        )
+        .dimensions(this.width / 2 - 75, this.height / 2 + 20, 150, 20)
+        .build());
     }
 
     private void addDebugMessage(Text text) {
-        assert this.client != null;
+        if (client == null) return;
         this.client.inGameHud.getChatHud().addMessage(Text.empty().append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD)).append(" ").append(text));
     }
 
