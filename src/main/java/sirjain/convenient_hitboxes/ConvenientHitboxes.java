96ef792ce2c02cc5f5be1a18200342036035d64b
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-09-30 18:36:31 -0400

    Cleanup, ensure mod works in 1.20.2

--- src/main/java/sirjain/convenient_hitboxes/ConvenientHitboxes.java
+++ src/main/java/sirjain/convenient_hitboxes/ConvenientHitboxes.java
@@ -5,7 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class ConvenientHitboxes implements ModInitializer {
 	public static final String MOD_NAME = "Convenient Hitboxes";
 	public static final String MOD_ID = "convenient_hitboxes";
-	public static final String MOD_VERSION = "1.0.0";
 
 	@Override
 	public void onInitialize() {
--- src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/sirjain/convenient_hitboxes/mixin/ControlsOptionsScreenMixin.java
@@ -3,7 +3,6 @@ package sirjain.convenient_hitboxes.mixin;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.screen.option.KeybindsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
@@ -15,37 +14,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ControlsOptionsScreen.class)
 public class ControlsOptionsScreenMixin extends GameOptionsScreen {
-    public ControlsOptionsScreenMixin(Screen parent, GameOptions gameOptions) {
-        super(parent, gameOptions, Text.translatable("hi"));
-    }
+	public ControlsOptionsScreenMixin(Screen parent, GameOptions gameOptions) {
+		super(parent, gameOptions, Text.translatable("hi"));
+	}
 
-    @Inject(at = @At("HEAD"), method = "init()V")
-    protected void init(CallbackInfo info) {
-        if (this.client == null) return;
-        System.out.println("Ctrls options screen");
+	@Inject(at = @At("HEAD"), method = "init()V")
+	protected void init(CallbackInfo info) {
+		if (this.client == null) return;
+		System.out.println("Ctrls options screen");
 
-        this.addDrawableChild(ButtonWidget.builder(
-            !this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"),
-            (button) -> {
-                this.client.getEntityRenderDispatcher().setRenderHitboxes(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes());
-                button.setMessage(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"));
-                this.debugLog(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? "debug.show_hitboxes.off" : "debug.show_hitboxes.on");
-            }
-        )
-        .dimensions(this.width / 2 - 75, this.height / 2 + 20, 150, 20)
-        .build());
-    }
+		this.addDrawableChild(ButtonWidget.builder(
+				!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"),
+				(button) -> {
+					this.client.getEntityRenderDispatcher().setRenderHitboxes(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes());
+					button.setMessage(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? Text.translatable("gui.entity_hitbox.disabled") : Text.translatable("gui.entity_hitbox.enabled"));
+					this.debugLog(!this.client.getEntityRenderDispatcher().shouldRenderHitboxes() ? "debug.show_hitboxes.off" : "debug.show_hitboxes.on");
+				}
+			)
+			.dimensions(this.width / 2 - 75, this.height / 2 + 20, 150, 20)
+			.build());
+	}
 
-    private void addDebugMessage(Text text) {
-        if (client == null) return;
-        this.client.inGameHud.getChatHud().addMessage(Text.empty().append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD)).append(" ").append(text));
-    }
+	private void addDebugMessage(Text text) {
+		if (client == null) return;
+		this.client.inGameHud.getChatHud().addMessage(Text.empty().append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD)).append(" ").append(text));
+	}
 
-    private void debugLog(Text text) {
-        this.addDebugMessage(text);
-    }
+	private void debugLog(Text text) {
+		this.addDebugMessage(text);
+	}
 
-    private void debugLog(String key, Object... args) {
-        this.debugLog(Text.translatable(key, args));
-    }
+	private void debugLog(String key, Object... args) {
+		this.debugLog(Text.translatable(key, args));
+	}
 }
